71ebea36bd8ff0ba39def32aa6b336dc261e057a
steven <engineers2526@gmail.com>
2021-07-25 12:17:54 -0500

    initial commit

---
+++ src/main/java/com/beer/core/beerservicemicrocourse/BeerServiceMicroCourseApplication.java
@@ -0,0 +1,13 @@
+package com.beer.core.beerservicemicrocourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceMicroCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerServiceMicroCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beer/core/beerservicemicrocourse/BeerServiceMicroCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.core.beerservicemicrocourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceMicroCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
